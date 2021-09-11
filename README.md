991f69ad7ac71c634ddd33b2b8cabe0d71955150
AlexKubbinga <alexkubbinga18@gmail.com>
2021-09-11 13:08:44 +0100

    Academic

--- README.md
+++
@@ -1,3 +0,0 @@
-# my_website
-
-#test
--- Script.R
+++
@@ -1,8 +0,0 @@
-library(blogdown)
-blogdown::install_hugo()
-
-blogdown::new_site(theme = "MarcusVirg/forty",
-                   sample = TRUE,
-                   theme_example = TRUE,
-                   empty_dirs = TRUE,
-                   to_yaml = TRUE)
